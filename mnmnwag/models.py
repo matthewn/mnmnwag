4e35e49320e44e7b074544a9e3b8a15038b18a89
Matthew Newton <matthewn@berkeley.edu>
2019-08-11 16:10:21 -0700

    remove unused imports

--- mnmnwag/models.py
+++ mnmnwag/models.py
@@ -1,31 +1,18 @@
-import datetime
-
-from django import forms
 from django.db import models
-from django.db.models import Q
-from django.template.response import TemplateResponse
 
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
 from wagtail.core import blocks
-from wagtail.core.models import Page, Orderable
+from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import (
     FieldPanel,
-    FieldRowPanel,
-    InlinePanel,
-    MultiFieldPanel,
     StreamFieldPanel
 )
-from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.snippets.edit_handlers import SnippetChooserPanel
-from wagtail.snippets.models import register_snippet
 
-from modelcluster.fields import ParentalKey, ParentalManyToManyField
-from modelcluster.models import ClusterableModel
+from modelcluster.fields import ParentalKey
 
 
 # ···························································
