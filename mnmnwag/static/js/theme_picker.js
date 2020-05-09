8bef4678ed6f8f3640523d86662847e340a5f9fb
Matthew Newton <matthewn@berkeley.edu>
2020-05-09 15:07:03 -0700

    use unpoly for event listeners (fixes ajax-related bug)

--- mnmnwag/static/js/theme_picker.js
+++ mnmnwag/static/js/theme_picker.js
@@ -5,13 +5,18 @@ const links = '#theme-picker li a';
 const prefix = 'theme-';
 
 
+// on page load, enable cookied theme (if it exists)
 document.addEventListener('DOMContentLoaded', function() {
-    [].forEach.call(document.querySelectorAll(links), function(link) {
-        link.addEventListener('click', changeTheme);
-    });
     if (getCookie('themeClass')) setTheme(getCookie('themeClass'));
     else markCurrentTheme();
 });
+
+
+// use unpoly to attach event listeners to theme picker links
+// (both on page load & after subsequent AJAX calls)
+up.compiler(links, function(link) {
+    link.addEventListener('click', changeTheme);
+})
 
 
 const getTheme = function() {
