019da63dfdfcec07a62d6a8131706a578bcfdebb
Matthew Newton <matthewn@berkeley.edu>
2020-08-16 15:53:28 -0700

    clear unpoly console splash on page load

--- mnmnwag/static/js/misc.js
+++ mnmnwag/static/js/misc.js
@@ -1,5 +1,6 @@
 // on page load, inform the document that javascript is available
 document.addEventListener('DOMContentLoaded', function() {
     up.modal.config.history = false;
+    console.clear();
 });
 
