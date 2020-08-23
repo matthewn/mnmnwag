2a7eafd459b6a02be216334b36536a0cb6330bd3
Matthew Newton <matthewn@berkeley.edu>
2020-08-23 15:07:48 -0700

    implement google analytics

--- mnmnwag/static/js/misc.js
+++ mnmnwag/static/js/misc.js
@@ -4,3 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     console.clear();
 });
 
+
+// update google analytics when unpoly link is followed
+up.on('up:link:follow ', function(event) {
+    target_page = event.target.attributes.href.value;
+    gtag('config', 'UA-176045696-1' , {'page_path': target_page});
+});
