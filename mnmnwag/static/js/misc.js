f02805d5aa124b0f51fceb6aecbf731bbd35d8e4
Matthew Newton <matthewn@berkeley.edu>
2020-08-30 22:54:19 -0700

    add hightlight.js for syntax highlighting in blog posts

--- mnmnwag/static/js/misc.js
+++ mnmnwag/static/js/misc.js
@@ -1,12 +1,20 @@
-// misc things to do on initial page load
+// on initial page load...
 document.addEventListener('DOMContentLoaded', function() {
     up.modal.config.history = false;
+    hljs.configure({useBR: true});
     console.clear();
 });
 
 
-// update google analytics when unpoly link is followed
-up.on('up:link:follow ', function(event) {
+// when any unpoly link is followed...
+up.on('up:link:follow', function(event) {
+    // update google analytics
     target_page = event.target.attributes.href.value;
     gtag('config', 'UA-176045696-1' , {'page_path': target_page});
 });
+
+
+// trigger highlight.js when needed
+up.compiler('.blog-post code', function(element) {
+    hljs.highlightBlock(element);
+})
