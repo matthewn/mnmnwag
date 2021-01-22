2de282e641af5c79aac6b912893ec48c09faff12
Matthew Newton <matthewn@berkeley.edu>
2021-01-21 20:47:21 -0800

    build out import some more

--- mnmnwag/management/commands/blosxom_import.py
+++ mnmnwag/management/commands/blosxom_import.py
@@ -1,6 +1,16 @@
+"""
+Import posts and comments from a blosxom blog with the writebacks plugin.
+
+Converts blosxom categories to tags.
+
+Assumes that TARGET_DIR is a directory tree containing blosxom blog posts
+(in .blog files) with writeback files (.wb) alongside them.
+"""
+
 from django.core.management.base import BaseCommand
 
 import os
+import re
 import time
 
 
@@ -8,25 +18,60 @@ TARGET_DIR = '/home/matthewn/working/01_PERSONAL/blosxom_data'
 
 
 class Command(BaseCommand):
-    help = ''
+    help = 'import a directory tree of blosxom and writeback files'
 
     def handle(self, *args, **options):
-        for (root, dirs, files) in os.walk(TARGET_DIR):
-            path = root.replace(TARGET_DIR, '')
+        for (dirname, subdirs, files) in os.walk(TARGET_DIR):
+            # get original relative site path
+            path = dirname.replace(TARGET_DIR, '')
+            # wagtail tag is tail directory from path
             tag = path.split('/')[-1]
+
             for filename in files:
                 if filename.endswith('.blog'):
-                    filepath = os.path.join(root, filename)
+                    filepath = os.path.join(dirname, filename)
                     with open(filepath, 'r') as f:
                         post = f.readlines()
 
-                    old_path = os.path.join(path, filename)
+                    # get post title
                     title = post[0][0:-1]
-                    # body = '\r'.join(post[1:])
+                    # convert old filename to new slug
+                    slug = filename.replace('.blog', '').replace('_', '-')
+                    # get post creation datetime
                     created = time.ctime(os.path.getmtime(filepath))
+                    # get old relative URL (for redirects on new site)
+                    old_url = os.path.join(path, filename).replace('.blog', '.html')
+                    # get body
+                    body = '\r'.join(post[1:])
+                    # does this post have comments?
                     has_comments = os.path.exists(filepath.replace('.blog', '.wb'))
 
-                    print(f'{old_path} {created} {tag} {title}')
-                    # print(body)
+                    print(f'\n{title}\n{slug}\n{created}\n{old_url}\n{tag}\n{body[:200]} ...')
+
                     if has_comments:
-                        print('HAS COMMENTS!!!!!!')
+                        with open(filepath.replace('.blog', '.wb')) as f:
+                            writebacks = f.readlines()
+                        comments = self.get_comments(writebacks)
+                        if comments:
+                            print(comments)
+
+    def get_comments(self, writebacks):
+        """
+        Return a list of dictionaries, each one containing a comment
+        for a post.
+        """
+        comments = []  # holds all comments
+        comment = {}   # holds one comment
+        for line in writebacks:
+            if '-----' not in line:
+                # store this line's data in the dictionary
+                regex = '^(.*): (.*)\n'
+                match = re.search(regex, line)
+                if match and match.group(2) != '':
+                    comment[match.group(1)] = match.group(2)
+            else:
+                # we have reached the end of a comment, so add it
+                # to the dictionary and start a new one
+                comments.append(comment)
+                comment = {}
+        return comments
