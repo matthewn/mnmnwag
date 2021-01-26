442b070e7bb10afa108c2cfaaa3e5112528f9c96
Matthew Newton <matthewn@berkeley.edu>
2021-01-25 22:15:33 -0800

    refactor, complete page imports

--- mnmnwag/management/commands/blosxom_import.py
+++ mnmnwag/management/commands/blosxom_import.py
@@ -8,12 +8,15 @@ Assumes that TARGET_DIR is a directory tree containing blosxom blog posts
 """
 
 from django.core.management.base import BaseCommand
+from django.utils.timezone import make_aware
+from wagtail.core.models import Page
+from mnmnwag.models import LegacyPost
 
+import datetime as dt
 import os
 import re
-import time
 
-
+PARENT_PAGE_ID = 4
 TARGET_DIR = '/home/matthewn/working/01_PERSONAL/blosxom_data'
 
 
@@ -22,38 +25,58 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for (dirname, subdirs, files) in os.walk(TARGET_DIR):
-            # get original relative site path
-            path = dirname.replace(TARGET_DIR, '')
-            # wagtail tag is tail directory from path
-            tag = path.split('/')[-1]
-
             for filename in files:
-                if filename.endswith('.blog'):
+                if filename.endswith('5books.blog'):  # FIXME
                     filepath = os.path.join(dirname, filename)
-                    with open(filepath, 'r') as f:
-                        post = f.readlines()
 
-                    # get post title
-                    title = post[0][0:-1]
-                    # convert old filename to new slug
-                    slug = filename.replace('.blog', '').replace('_', '-')
-                    # get post creation datetime
-                    created = time.ctime(os.path.getmtime(filepath))
-                    # get old relative URL (for redirects on new site)
-                    old_url = os.path.join(path, filename).replace('.blog', '.html')
-                    # get body
-                    body = '\r'.join(post[1:])
+                    page = self.build_page(filepath)
+                    self.publish_page(**page)
+
                     # does this post have comments?
                     has_comments = os.path.exists(filepath.replace('.blog', '.wb'))
-
-                    print(f'\n{title}\n{slug}\n{created}\n{old_url}\n{tag}\n{body[:200]} ...')
 
                     if has_comments:
                         with open(filepath.replace('.blog', '.wb')) as f:
                             writebacks = f.readlines()
                         comments = self.get_comments(writebacks)
                         if comments:
-                            print(comments)
+                            # print(comments)
+                            pass
+
+    def build_page(self, filepath):
+        page = {}
+        with open(filepath, 'r') as f:
+            post = f.readlines()
+        # get post title
+        page['title'] = post[0][0:-1]
+        # convert old filename to new slug
+        page['slug'] = filepath.split('/')[-1].replace('.blog', '').replace('_', '-')
+        # get post creation datetime
+        page['created'] = make_aware(dt.datetime.fromtimestamp(os.path.getmtime(filepath)))
+        # get old relative URL (for redirects on new site)
+        page['old_path'] = filepath.replace(TARGET_DIR, '').replace('.blog', '.html')
+        # get wagtail tag (tail directory from path)
+        page['tag'] = filepath.split('/')[-2]
+        # get body
+        page['body'] = '\r'.join(post[1:])
+        return page
+
+    def publish_page(self, title, slug, created, old_path, tag, body):
+        # print(f'\n{title}\n{slug}\n{created}\n{old_path}\n{tag}\n{body[:200]} ...')
+        print(title)
+        parent_page = Page.objects.get(id=PARENT_PAGE_ID).specific
+        page = LegacyPost(  # foooo
+            title=title,
+            body=body,
+            old_path=old_path,
+            slug=slug,
+            has_comments_enabled=False,
+            first_published_at=created,
+        )
+        page.tags.add(tag)
+        page.tags.add('from-blosxom')
+        parent_page.add_child(instance=page)
+        page.save_revision().publish()
 
     def get_comments(self, writebacks):
         """
