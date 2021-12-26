af1d3baa2cdb4553920dbaa25b4ee2d2971c1432
Matthew Newton <matthewn@berkeley.edu>
2021-12-26 15:29:33 -0800

    switch from google analytics to goatcounter

--- mnmnwag/static/js/main.js
+++ mnmnwag/static/js/main.js
@@ -58,9 +58,8 @@ up.macro('[up-nav-link]', function(link) {
 
 // when any unpoly link is followed...
 up.on('up:link:follow', function(event) {
-    // update google analytics
-    target_page = event.target.attributes.href.value;
-    gtag('config', 'UA-176045696-1' , {'page_path': target_page});
+    // ping goatcounter
+    window.goatcounter.count({path: event.target.attributes.href.value});
 });
 
 
