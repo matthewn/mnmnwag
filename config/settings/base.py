993d231d1c99587c4b6af162152bdbf48aa778c0
Matthew Newton <matthewn@berkeley.edu>
2022-01-14 23:12:47 -0800

    update BASE_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -167,7 +167,7 @@ WAGTAILMEDIA_MEDIA_MODEL = 'mnmnwag.CustomMedia'
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'https://new.mahnamahna.net'
+BASE_URL = 'https://www.mahnamahna.net'
 
 WAGTAILSEARCH_BACKENDS = {
     'default': {
