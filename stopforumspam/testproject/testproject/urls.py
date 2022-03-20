23d2c2144a1991f78ad978c95260eafcc536fc60
Matthew Newton <matthewn@berkeley.edu>
2022-03-20 16:32:49 -0700

    fix busted vendoring

--- stopforumspam/testproject/testproject/urls.py
+++
@@ -1,3 +0,0 @@
-# from django.conf.urls import include, patterns, url
-
-urlpatterns = []
---
+++ typogrify/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from typogrify.tests.test_fuzzydate import *
+from typogrify.tests.test_tags import *
+from typogrify.tests.test_titlecase import *
+
+
+__all__ = ['TestFuzzydate', 'TestTitlecase']
