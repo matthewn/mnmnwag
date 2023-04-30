70f9f13a478d6fa219164afa0872a90979714fba
Matthew Newton <matthewn@mahnamahna.net>
2023-04-29 21:53:48 -0700

    simplify code

--- madprops/models.py
+++ madprops/models.py
@@ -34,19 +34,18 @@ class EditionPage(BasePage):
     content_panels = Page.content_panels + [
         FieldPanel('body'),
         FieldPanel('has_comments_enabled'),
+        FieldPanel('first_published_at'),
     ]
 
     def cheat_sheet(self):
-        data = []
-        for block in self.body:
-            if block.block_type == 'prop':
-                row = {
-                    'prop_number': block.value['prop_number'],
-                    'prop_title': block.value['prop_title'],
-                    'recommendation': block.value['recommendation'],
-                }
-                data.append(row)
-        return data
+        return [
+            {
+                'prop_number': block.value['prop_number'],
+                'prop_title': block.value['prop_title'],
+                'recommendation': block.value['recommendation'],
+            }
+            for block in self.body if block.block_type == 'prop'
+        ]
 
     def get_absolute_url(self):
         """
