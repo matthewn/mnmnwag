b04114862f0224aaf96c68bf87dfc3bfb3ad7426
Matthew Newton <matthewn@mahnamahna.net>
2023-11-04 17:45:49 -0700

    implement backtotop

---
+++ madprops/static/madprops/js/main.js
@@ -0,0 +1,16 @@
+console.log('FUCK YOU');
+
+const backtotop = document.getElementById('backtotop');
+
+function toggleBacktotopVisibility() {
+    const scrollY = window.scrollY || window.pageYOffset;
+    const elementTop = backtotop.offsetTop;
+
+    if (scrollY > elementTop - (window.innerHeight / 2) + 200) {
+        backtotop.classList.add('visible');
+    } else {
+        backtotop.classList.remove('visible');
+    }
+}
+
+window.addEventListener('scroll', toggleBacktotopVisibility);
