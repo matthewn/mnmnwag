1d14758a995b3bab4dff977348a3293cb9309482
Matthew Newton <matthewn@mahnamahna.net>
2025-01-12 16:07:08 -0800

    add support for heic img uploads

--- config/settings/base.py
+++ config/settings/base.py
@@ -178,6 +178,7 @@ WAGTAILEMBEDS_RESPONSIVE_HTML = True
 WAGTAILIMAGES_IMAGE_MODEL = 'mnmnwag.CustomImage'
 WAGTAILMEDIA_MEDIA_MODEL = 'mnmnwag.CustomMedia'
 WAGTAILIMAGES_MAX_UPLOAD_SIZE = 20 * 1024 * 1024  # 20MB
+WAGTAILIMAGES_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'png', 'webp', 'heic']
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
