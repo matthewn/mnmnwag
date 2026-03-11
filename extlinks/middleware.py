1fe7c262d9fc205c74991e18549dfe77835ff423
Matthew Newton <matthewn@mahnamahna.net>
2026-03-11 14:07:44 -0700

    refactor extlinks to prevent unnecessary re-rendering

--- extlinks/middleware.py
+++ extlinks/middleware.py
@@ -5,11 +5,14 @@ import re
 ignore_domains = getattr(settings, 'EXTLINKS_IGNORE_DOMAINS', ())
 templates_whitelist = getattr(settings, 'EXTLINKS_TEMPLATES_WHITELIST', ())
 
+link_regex = re.compile(r'<\s*a[^>]*>')
+ignore_strings = ['://' + domain for domain in ignore_domains]
+
 
 class RewriteExternalLinksMiddleware(object):
     """
     Rewrite all external links to open in a new tab/window.
-    (A somewhat (?) naive implementation, expected to improve over time.)
+    (In which we commit a sin by altering HTML with a regex -- but it's okay! Really!)
 
     Rewrite:
         <a href="http://www.example.com">
@@ -31,22 +34,18 @@ class RewriteExternalLinksMiddleware(object):
         if not templates_whitelist:
             return response
 
-        link_regex = re.compile('<\s*a[^>]*>')
-        new_content = response.rendered_content
-        ignore_strings = ['://' + domain for domain in ignore_domains]
-
-        try:
-            matches = link_regex.findall(new_content)
-        except TypeError:
+        if not any(path in response.template_name for path in templates_whitelist):
             return response
 
-        if any(path in response.template_name for path in templates_whitelist):
-            for match in matches:
-                if all(item not in match for item in ignore_strings):
-                    if '://' in match and 'target=' not in match:
-                        new_link = match.replace(' href=', ' target="_blank" href=')
-                        new_content = new_content.replace(match, new_link, 1)
-
-        response.content = new_content
-
+        response.add_post_render_callback(self._rewrite_links)
         return response
+
+    @staticmethod
+    def _rewrite_links(response):
+        content = response.content.decode(response.charset)
+        for match in link_regex.findall(content):
+            if all(item not in match for item in ignore_strings):
+                if '://' in match and 'target=' not in match:
+                    new_link = match.replace(' href=', ' target="_blank" href=')
+                    content = content.replace(match, new_link, 1)
+        response.content = content
