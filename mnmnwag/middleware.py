8de18ec9e2710981742b5da89573211ac4abdcf8
Matthew Newton <matthewn@mahnamahna.net>
2026-03-20 13:20:10 -0700

    remove long unused, obsolete middleware

--- mnmnwag/middleware.py
+++ mnmnwag/middleware.py
@@ -42,25 +42,3 @@ class ThemeClassCacheMiddleware:
         response = self.get_response(request)
         patch_vary_headers(response, ['X-Theme-Class'])
         return response
-
-
-class AddUnpolyHeadersMiddleware(object):
-    """
-    Django reimplementation of
-    https://github.com/unpoly/unpoly-rails/blob/master/lib/unpoly/rails/request_echo_headers.rb
-
-    Echoes the request's URL as a response header `X-Up-Location` and the
-    request's HTTP method as `X-Up-Method`.
-
-    The Unpoly frontend requires these headers to detect redirects, which are
-    otherwise undetectable for an AJAX client.
-    """
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response['X-Up-Location'] = request.get_raw_uri()
-        response['X-Up-Method'] = request.method
-        return response
