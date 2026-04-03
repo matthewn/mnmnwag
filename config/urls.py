585dea7f1f9596532b001d7a484bd5bd35506701
Matthew Newton <matthewn@mahnamahna.net>
2026-04-03 10:35:37 -0700

    integrate seevooplay 2.x

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,6 @@ from wagtail_footnotes import urls as footnotes_urls
 from mnmnwag.feeds import LatestEntriesFeed
 from mnmnwag.views import theme_picker, zoom_image, zoom_old, zoom_slide
 
-from seevooplay.views import email_guests, event_page, resend_page
 from subs.views import sub_create, sub_confirm, sub_remove
 
 
@@ -52,23 +51,12 @@ urlpatterns = [
     path('sub/<str:uuid>/confirm', sub_confirm, name='sub_confirm'),
     path('sub/<str:uuid>/remove', sub_remove, name='sub_remove'),
 
-    path('dja/email_guests/<int:event_id>/', email_guests, name='email_guests'),
-    path('rsvp/', resend_page, name='resend'),
-    path('rsvp/<int:event_id>/', event_page, name='invitation'),
-    path('rsvp/<int:event_id>/<guest_uuid>/', event_page),
-    path('djrichtextfield/', include('djrichtextfield.urls')),
+    path('events/', include('seevooplay.urls')),
 
     path('blog/feed/<str:tag_ids>/', LatestEntriesFeed(), name='feed_tags'),
     path('blog/feed/', LatestEntriesFeed(), name='feed'),
 
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
     path('', include(wagtail_urls)),
-
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
 ]
 
 handler404 = 'mnmnwag.views.handler404'
@@ -78,7 +66,7 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server
+    # serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
